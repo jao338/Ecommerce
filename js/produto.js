597bfb100853887e9809ef9f9848eb5f6a2595ba
João Henrique <santos.joao@crmsolucoes.com.br>
2023-09-20 11:03:05 -0300

    Ajuste

--- js/produto.js
+++ js/produto.js
@@ -1,7 +1,25 @@
-let arrow = document.querySelector('.arrowDesc');
-let descP = document.querySelector('#desc-produto');
+let toggleInfo = document.querySelector('.dropInfo');
+let toggleDesc = document.querySelector('.dropDesc');
+let secInfo = document.querySelector('.sec-info');
+let secDesc = document.querySelector('.sec-desc');
 
-arrow.addEventListener('click', () => {
 
+toggleInfo.addEventListener('click', () => {
 
+    if(secInfo.style.height == '685px'){
+        secInfo.style.height = 'auto'; 
+    }else{
+        secInfo.style.height = '685px'; 
+    } 
+    
+});
+
+toggleDesc.addEventListener('click', () => {
+
+    if(secDesc.style.height == '224px'){
+        secDesc.style.height = 'auto'; 
+    }else{
+        secDesc.style.height = '224px'; 
+    } 
+    
 });
